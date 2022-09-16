4c5d1bc1a6c2927eb47d7764bf3e827595d90923
naveenkumar <naveenkumarlanka77@gmail.com>
2022-09-16 07:45:44 +0000

    creating a user list using reactjs

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import UserProfile from './components/UserProfile'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Naveen Kumar',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'sonia',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'fathima',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'jack',
+    role: 'Software Developer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetailsList.map(eachitem => (
+        <UserProfile userDetails={eachitem} key={eachitem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+// rendering
+
+export default App
